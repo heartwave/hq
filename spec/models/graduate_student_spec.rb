bdfacd95a773fb92a16f412a6ded7b762134dd3b
Kate <pavlintseva.e.d@gmail.com>
2014-05-26 18:58:28 +0400

    graduate student model

---
+++ spec/models/graduate_student_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe GraduateStudent do
+ 
+  describe 'обладает связями с другими моделями:' do
+    it 'с Graduate' do
+      should belong_to(:graduate)
+    end
+
+     it 'со студентами' do
+      should belong_to(:student)
+    end
+    it 'с выпускными оценками' do
+      should have_many(:graduate_marks)
+    end
+  end
+
+end
